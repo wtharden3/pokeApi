c376f5a13fe0719c6851acb606dd06636bac41fa
Whitney Harden <wtharden320@gmail.com>
2020-10-22 11:39:33 -0400

    script changes-assigning attributes and second api modification

--- script.js
+++ script.js
@@ -1,65 +1,60 @@
 let pokeAPI = 'https://pokeapi.co/api/v2/pokemon';
 let pokeDex = 'https://pokeapi.co/api/v2/pokedex/1';
-let nextPoke20;
-let prevPoke20;
 let pokeUl = document.querySelector('.pokeUl');
 console.log('pokeUl: ', pokeUl);
 
-// let fetchApi = async () => {
-//   fetch(pokeAPI)
-//     .then(response => {
-//       response.json();
-//     })
-//     .then(json => console.log(json))
-//     .catch(err => console.log(err));
-//   console.log("What's up");
-// };
-
+//The fetchPokeDex function declaration START below
 let fetchPokeDex = async api => {
   const response = await fetch(api);
   const pokeJson = await response.json();
+
   let names = [];
-  for (let i = 0; i <= 151; i++) {
-    names.push(pokeJson.pokemon_entries[i].pokemon_species.name);
+
+  for (let i = 0; i <= 150; i++) {
+    pokeUl.appendChild(document.createElement('li')).innerText =
+      pokeJson.pokemon_entries[i].pokemon_species.name;
+    pokeUl
+      .appendChild(document.createElement('div'))
+      .setAttribute('class', 'collapse');
+    names.push(pokeJson.pokemon_entries[i].pokemon_species);
   }
+  let lis = document.querySelectorAll('li');
+  //let divs = document.querySelectorAll('')
+  // loop through all the nodes and assign a class attribute
+
+  //testing something commenting this out for now to see if I can chain setAttribute above will still need this
+  for (li of lis) {
+    let classValue = `list-`;
+    li.setAttribute('data-toggle', 'collapse');
+    console.log(li);
+  }
+
+  console.log(lis[1]);
+  console.log('names: ', names[0]);
+  //we are returning an array of objects.
+  //names will return {name: 'pokemonname', url: 'https://apiforthatpokemon'}
   return names;
-  
-  
-  // return pokeJson.pokemon_entries[0].pokemon_species.name;
-  
-  //return pokeJson.pokemon_entries.length;
 };
+//The fetchPokeDex function declaration END below
 
 fetchPokeDex(pokeDex)
-  .then(console.log)
+  .then(data => {
+    console.log('data[1].name: ', data[1].name, 'data[1].url', data[1].url);
+    //details about specific pokemon below
+    //nedd to loop conditionally
+    //match two variables
+    //loop through data[i].name and data[i].url
+    let lis = document.querySelectorAll('li');
+    // loop through all the nodes and assign a class attribute
+    //console.log('lis: ', lis);
+    innerFetch(data[0].url)
+      .then(console.log)
+      .catch(err => console.log(err));
+  })
   .catch(err => console.log(err));
 
-let fetchApi = async api => {
-  const response = await fetch(api);
-  const pokeJson = await response.json();
-  let count = pokeJson.count;
-  nextPoke20 = pokeJson.next;
-  prevPoke20 = pokeJson.previous;
-  console.log('count: ', count);
-  console.log('pokeJson: ', pokeJson);
-  console.log('next: ', nextPoke20);
-  console.log('previous: ', prevPoke20);
-  //console.log('length', pokeJson.results.length);
-  let results = pokeLoop(pokeJson.results);
-  return results;
-};
-
-fetchApi(pokeAPI)
-  .then(console.log)
-  .catch(err => console.log(err));
-
-//let practiceArr = ['Will', 'you', 'work', '?', 100];
-//console.log(pokeLoop(practiceArr));
-
-function pokeLoop(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    pokeUl.appendChild(document.createElement('li')).innerText = arr[i].name;
-    let li = document.querySelectorAll('li');
-    //console.log(arr[i]);
-  }
+async function innerFetch(api) {
+  const res = await fetch(api);
+  const json = await res.json();
+  return json;
 }
